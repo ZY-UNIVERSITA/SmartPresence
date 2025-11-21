a05ad960df3da96ebf22b851489234d606432ca2
ZY-UNIVERSITA <yuhang.zhu@studio.unibo.it>
2025-11-22 00:57:02 +0100

    Move area class

--- src/SmartPresence/Services/Areas/Area.cs
+++ src/SmartPresence/Services/Areas/Area.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace SmartPresence.Services.Shared
+namespace SmartPresence.Services.Areas
 {
     public class Area
     {
