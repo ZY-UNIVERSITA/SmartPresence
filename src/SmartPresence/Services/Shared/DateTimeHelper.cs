a7dc75c486a5e3318146410a66ee3795b854776d
ZY-UNIVERSITA <yuhang.zhu@studio.unibo.it>
2025-11-24 23:18:13 +0100

    Add date time helper

---
+++ src/SmartPresence/Services/Shared/DateTimeHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartPresence.Services.Shared
+{
+    public static class DateTimeHelper
+    {
+        public static DateTime GetMonday(DateTime date)
+        {
+            // calcolo la distanza dal lunedi
+            // c# usa 0 per sunday fino a 6 per saturday
+            var mondayOff = DayOfWeek.Monday - date.DayOfWeek;
+
+            // perciò se oggi cade di domenica allora si avrebbe 1-0 = 1 quindi per ottenere domenica ovvero 6 (0 = lunedy e 6 = domenica)
+            // bisogna sottrarre -7.
+            // 1 - 7 = -6 che andrà aggiunto alla data passata per ottenere il lunedi
+            if (mondayOff.Equals(1))
+            {
+                mondayOff -= 7;
+            }
+
+            var mondayDate = date.AddDays(mondayOff);
+
+            return mondayDate;
+        }
+
+        public static DateTime GetFirstOfTheMonth(DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, 1);
+        }
+    }
+    }
+}
