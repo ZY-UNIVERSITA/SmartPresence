be61bd6e4ec6dda610e1e7f0b4b555b0b3fb150b
ZY-UNIVERSITA <yuhang.zhu@studio.unibo.it>
2025-11-28 09:37:38 +0100

    Add event filter by organization level

---
+++ src/SmartPresence/Services/Shared/OrganizationLevelFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartPresence.Services.Shared
+{
+    public enum OrganizationLevelFilter
+    {
+        PERSONAL,
+        TEAM,
+        AREA,
+        ALL
+    }
+}
