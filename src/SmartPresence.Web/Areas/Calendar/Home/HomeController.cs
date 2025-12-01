c42bd1a5cd63239a4eea247bf67e96dabceca38c
ZY-UNIVERSITA <yuhang.zhu@studio.unibo.it>
2025-12-01 11:47:02 +0100

    Add employee time off request Add create new work event request API

--- src/SmartPresence.Web/Areas/Calendar/Home/HomeController.cs
+++ src/SmartPresence.Web/Areas/Calendar/Home/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SmartPresence.Services.Employees;
+using SmartPresence.Services.Employees.Queries;
 using SmartPresence.Services.Users;
 using SmartPresence.Services.WorkEvents;
 using SmartPresence.Web.Infrastructure;
+using System;
 using System.Threading.Tasks;
 
 namespace SmartPresence.Web.Areas.Calendar.Home
@@ -43,19 +45,44 @@ namespace SmartPresence.Web.Areas.Calendar.Home
             var employeeWorkEventRequest = searchModel.ToQuery(userId);
             var queryResult = await _employeeService.GetAllEmployeeWorkEvents(employeeWorkEventRequest);
 
+            // Fai una seconda query per ottenere le statistiche dei permessi e delle ferie
+            var employeeTimeOffRequest = new EmployeeTimeOffByIdAndYearRequest()
+            {
+                Id = userId,
+                Year = DateTime.Now.Year
+            };
+            var queryResultTimeOff = await _employeeService.GetEmployeeTimeOff(employeeTimeOffRequest);
+
             // Passa i dati alla Index View Model che prepara i dati ottenuti dalla query per la view
             var viewModel = new HomeIndexViewModel()
             {
-                Id = userId,
+                IdEmployee = userId,
                 BeginDate = employeeWorkEventRequest.BeginDate,
                 EndDate = employeeWorkEventRequest.EndDate,
-                CalendarViewFilter = searchModel.CalendarView,
+                CalendarViewFilter = searchModel.CalendarView
             };
             viewModel.PrepareCalendarHeader();
-            viewModel.PrepareViewData(queryResult);
+            viewModel.PrepareViewData(queryResult, queryResultTimeOff);
 
             // Restituisci la pagina all'utente
             return View(viewModel);
         }
+
+        [HttpPost]
+        public virtual async Task<IActionResult> CreateNewRequest(NewRequestModel model)
+        {
+            // Crea un search model che rifletta la view dell'utente in quel momento
+            var searchModel = model.ToSearchmodel();
+
+            // Crea il comando per inserire una nuova data
+            var command = model.ToCommand();
+
+            // Aggiungi il nuovo work event
+            await _workEventService.CreateNewWorkEvent(command);
+
+            // Restituisci la stessa pagina in cui l'utente si trova
+            return RedirectToAction("Index", searchModel);
+        }
+
     }
 }
