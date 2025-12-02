cc99c0c812837da756802830329c50200aed7c38
ZY-UNIVERSITA <yuhang.zhu@studio.unibo.it>
2025-12-03 00:04:26 +0100

    Update work event model

--- src/SmartPresence/Services/WorkEvents/Model/WorkEvent.cs
+++ src/SmartPresence/Services/WorkEvents/Model/WorkEvent.cs
@@ -11,6 +11,7 @@ namespace SmartPresence.Services.WorkEvents.Model
         public int Id { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public string Notes { get; set; } = string.Empty;
 
         public int IdWorkEventStatus { get; set; }
 
