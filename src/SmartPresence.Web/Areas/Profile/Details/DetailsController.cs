4443b9c512fb74dd28167267dfadcb8a46b339d4
ZY-UNIVERSITA <yuhang.zhu@studio.unibo.it>
2025-12-03 15:04:23 +0100

    Add some code to test profile service

--- src/SmartPresence.Web/Areas/Profile/Details/DetailsController.cs
+++ src/SmartPresence.Web/Areas/Profile/Details/DetailsController.cs
@@ -1,14 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SmartPresence.Services.Employees;
 using SmartPresence.Services.Employees.Model;
+using SmartPresence.Services.Users;
+using System;
+using System.Threading.Tasks;
 
 namespace SmartPresence.Web.Areas.Profile.Details
 {
     [Area("Profile")]
-    public partial class DetailsController : Controller
+    public partial class DetailsController : AuthenticatedBaseController
     {
-        public virtual IActionResult Index()
+        public readonly IEmployeeService _employeeService;
+        public readonly IUserService _userService;
+
+        public DetailsController(IEmployeeService employeeService, IUserService userService)
         {
-            return View();
+            _employeeService = employeeService;
+            _userService = userService;
+        }
+
+        public virtual async Task<IActionResult> Index()
+        {
+            var email = Identita.EmailUtenteCorrente;
+            var userId = _userService.GetId(new Services.Users.Queries.UserIdentificationRequest(email));
+
+            var queryResult = await _employeeService.GetEmployeePersonalWorkEvent(new Services.Employees.Queries.EmployeePersonalWorkEventRequest()
+            {
+                IdEmployee = userId,
+                Year = DateTime.UtcNow.Year
+            });
+
+            return View(queryResult);
         }
     }
 }
