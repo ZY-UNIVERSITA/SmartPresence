b0d74c2591152a988b6a7af70e631f621f25006e
ZY-UNIVERSITA <yuhang.zhu@studio.unibo.it>
2025-12-05 11:55:32 +0100

    Add date time helper function to create a list of work days and holiday days

---
+++ src/SmartPresence/Services/Shared/CalendarDay.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartPresence.Services.Shared
+{
+    public class CalendarDay
+    {
+        public string DayOfWeek { get; set; }
+        public string NumberOfDay { get; set; }
+        public string Date { get; set; }
+        public DayType Holiday { get; set; }
+    }
+}
--- src/SmartPresence/Services/Shared/DateTimeHelper.cs
+++ src/SmartPresence/Services/Shared/DateTimeHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace SmartPresence.Services.Shared
 {
@@ -84,6 +86,97 @@ namespace SmartPresence.Services.Shared
                 _ => date,
             };
         }
+
+        // Funzione che serve per calcolare i giorni in cui sono presenti sabato, domenica e festività
+        public static List<CalendarDay> GetDaysWithHolidays(DateTime beginDate, DateTime endDate)
+        {
+            // Ottieni le festività dell'anno corrente ed eventualmente dell'anno successivo se l'enddate è in un altro anno
+            var holidaysBeginDate = Holidays.GetAllHolidaysByYear(beginDate.Year);
+            var holidaysEndDate = endDate.Year.Equals(beginDate.Year) ? holidaysBeginDate : Holidays.GetAllHolidaysByYear(endDate.Year);
+
+            // Ora si calcolano i giorni tra begin e end
+            var days = DateTimeHelper.GetDaysBetweenTwoDates(beginDate, endDate);
+
+            var list = new List<CalendarDay>();
+
+            // Per ognuno di questi giorni, si crea un oggetto contenente:
+            // giorno della settimana
+            // numero del mese
+            // data copmleta in formato yyyy-MM-dd
+            // se il giorno è lavorativo oppure è un festività
+            for (int i = 0; i < days; i++)
+            {
+                var currentDay = beginDate.AddDays(i);
+                var calendarDay = new CalendarDay()
+                {
+                    DayOfWeek = days <= DateTimeHelper.GetNumbersOfDaysInWeek()
+                        ? currentDay.ToString("ddd", CultureInfo.InvariantCulture).ToUpper()
+                        : currentDay.ToString("ddd", CultureInfo.InvariantCulture).Substring(0, 1).ToUpper(),
+
+                    // Numero del mese 
+                    NumberOfDay = currentDay.Day.ToString(),
+
+                    // Data completa in formato anno-mese-giorno
+                    Date = currentDay.Date.ToString("yyyy-MM-dd"),
+                    Holiday = holidaysBeginDate.Contains(currentDay) || holidaysEndDate.Contains(currentDay)
+                        || currentDay.DayOfWeek.Equals(DayOfWeek.Sunday) || currentDay.DayOfWeek.Equals(DayOfWeek.Saturday)
+                        ? DayType.HOLIDAY : DayType.WORK
+                };
+                list.Add(calendarDay);
+            }
+
+            return list;
+        }
     }
 
+    public static class Holidays
+    {
+        public static List<DateTime> GetAllHolidaysByYear(int year)
+        {
+            var easter = Holidays.GetEasterDate(year);    // Pasqua
+
+            return new List<DateTime>
+            {
+                new DateTime(year, 1, 1),   // 1 gennaio
+                new DateTime(year, 1, 6),   // 6 gennaio
+                new DateTime(year, 4, 25),  // 25 aprile
+                new DateTime(year, 5, 1),   // 1 maggio
+                new DateTime(year, 6, 2),   // 2 giugno
+                new DateTime(year, 8, 15),  // 15 agosto
+                new DateTime(year, 11, 1),  // 1 novembre
+                new DateTime(year, 12, 8),  // 8 dicembre
+                new DateTime(year, 12, 25), // 25 dicembre
+                new DateTime(year, 12, 26),  // 26 dicembre
+                easter,                     // pasqua
+                easter.AddDays(1)           // pasquetta
+            };
+        }
+
+        // Algoritmo di Gauss per calcolare pasqua
+        public static DateTime GetEasterDate(int year)
+        {
+            int a = year % 19;
+            int b = year / 100;
+            int c = year % 100;
+            int d = b / 4;
+            int e = b % 4;
+            int f = (b + 8) / 25;
+            int g = (b - f + 1) / 3;
+            int h = (19 * a + b - d - g + 15) % 30;
+            int i = c / 4;
+            int k = c % 4;
+            int l = (32 + 2 * e + 2 * i - h - k) % 7;
+            int m = (a + 11 * h + 22 * l) / 451;
+            int month = (h + l - 7 * m + 114) / 31;
+            int day = ((h + l - 7 * m + 114) % 31) + 1;
+
+            return new DateTime(year, month, day);
+        }
+    }
+
+    public enum DayType
+    {
+        WORK,
+        HOLIDAY
+    }
 }
