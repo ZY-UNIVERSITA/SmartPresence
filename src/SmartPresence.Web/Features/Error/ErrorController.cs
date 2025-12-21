94b75ba9209aa0a01369ab0c37de11236a2c309d
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2025-12-21 21:36:17 +0100

    Fix error page layout

--- src/SmartPresence.Web/Features/Error/ErrorController.cs
+++ src/SmartPresence.Web/Features/Error/ErrorController.cs
@@ -5,23 +5,9 @@ namespace SmartPresence.Web.Features.Error
 {
     public partial class ErrorController : Controller
     {
-        // Action principale per errori generici
-        public virtual IActionResult Index()
-        {
-            return View("PageNotFound");
-        }
-
         [Route("Error/PageNotFound")]
         public virtual IActionResult PageNotFound()
         {
-            Response.StatusCode = 404;
-            return View("PageNotFound");
-        }
-
-        [Route("Error/AccessDenied")]
-        public virtual IActionResult AccessDenied()
-        {
-            Response.StatusCode = 403;
             return View("PageNotFound");
         }
     }
